b25232e9a338aaad96cdb57e076880d77f9fa347
Martin Schwarz <martin.schwarz10@gmail.com>
2015-09-27 01:40:28 +0200

    bugfix

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ chmod a+x ./install-scripts/install.sh
 chmod a+x ./system-setup/capl_lock_to_escape_shell.sh
 chmod a+x ./system-setup/capl_lock_to_escape_x.sh
 
+echo "Executing installscripts"
+
 ./install-scripts/install.sh
 ./system-setup/capl_lock_to_escape_shell.sh
 ./system-setup/capl_lock_to_escape_x.sh
@@ -17,16 +19,22 @@ chmod a-x ./install-scripts/install.sh
 chmod a-x ./system-setup/capl_lock_to_escape_shell.sh
 chmod a-x ./system-setup/capl_lock_to_escape_x.sh
 
+echo "Copying files"
+
 cp -r ./dotfiles/.moc ~/.moc
 
 cp ./dotfiles/.vimrc ~/.vimrc
 cp -r ./dotfiles/.vim ~/.vim
+
+echo "Installing vim plugins"
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-$curdir = pwd
+curdir=pwd
 cd ~/.vim/bundle/command-t/ruby/command-t
 ruby extconf.rb
 make
 cd $curdir
 
+echo "Finished"
