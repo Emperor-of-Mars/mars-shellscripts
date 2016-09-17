ff3786c200f6767cf665aaa090fae6afba771d99
Martin Schwarz <martin.schwarz10@gmail.com>
2016-09-17 21:43:17 +0200

    virtualbox

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -2,7 +2,8 @@
 
 apt-get install -y build-essential yakuake libsdl2-* libglew-dev glew-utils \
 chromium-browser chromium-browser-l10n kdevelop git cmake ruby-dev clang \
-libreoffice audacious libreoffice vlc git virtualbox-nonfree vim-nox unetbootin \
-exuberant-ctags moc vifm redshift thunderbird thunderbird-locale-de ant blender
+libreoffice audacious libreoffice vlc git virtualbox vim-nox unetbootin \
+exuberant-ctags moc vifm redshift thunderbird thunderbird-locale-de ant blender \
+binfmt-support
 
 #pepperflashplugin-nonfree
